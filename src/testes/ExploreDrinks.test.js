fadaeb8f6ed4d5da479a173b49f34471dc7f28e4
Lucas Dalbo Fernandes <dalbo.lucas@gmail.com>
2022-06-30 17:26:19 -0300

    [Grupo 2] Mais testes

--- src/testes/ExploreDrinks.test.js
+++ src/testes/ExploreDrinks.test.js
@@ -3,8 +3,12 @@ import userEvent from '@testing-library/user-event';
 import React from 'react';
 import App from '../App';
 import renderWithRouter from './helpers/renderWithRouter';
+import mockRandomDrink from './helpers/mockRandomDrink';
+import mockDrinksIngredients from './helpers/mockDrinksIngredients';
 
 const path = '/explore/drinks';
+const URL = 'https://www.thecocktaildb.com/api/json/v1/1/random.php';
+const LENGTH = 12;
 
 describe('Testa a página Explore Drinks', () => {
   test('Verifica se existem os 2 botões de acordo com o protótipo', () => {
@@ -26,10 +30,10 @@ describe('Testa a página Explore Drinks', () => {
     const { history } = renderWithRouter(<App />);
     history.push(path);
 
-    const exploreFoodsButton = screen.getByRole('button', {
+    const ingredientBtn = screen.getByRole('button', {
       name: /by ingredient/i,
     });
-    userEvent.click(exploreFoodsButton);
+    userEvent.click(ingredientBtn);
     expect(history.location.pathname).toBe('/explore/drinks/ingredients');
   });
 
@@ -37,10 +41,45 @@ describe('Testa a página Explore Drinks', () => {
     const { history } = renderWithRouter(<App />);
     history.push(path);
 
+    const fetch = jest.spyOn(global, 'fetch');
+    fetch.mockResolvedValue({
+      json: jest.fn().mockResolvedValue(mockRandomDrink),
+    });
+
     const surpriseMeButton = screen.getByRole('button', {
       name: /surprise me/i,
     });
     userEvent.click(surpriseMeButton);
-    expect(history.location.pathname).toMatch(/drinks/);
+    expect(fetch).toBeCalledWith(URL);
+
+    setTimeout(() => {
+      expect(history.location.pathname).toMatch('/drinks/17120');
+    });
+
+    fetch.mockRestore();
+  });
+
+  test('A tela de explorar por ingrediente exibe os ingredientes', async () => {
+    const fetch = jest.spyOn(global, 'fetch');
+    fetch.mockResolvedValueOnce({
+      json: jest.fn().mockResolvedValue(mockDrinksIngredients),
+    });
+    const { history } = renderWithRouter(<App />);
+    history.push(path);
+
+    const byIngredientButton = screen.getByRole('button', {
+      name: /by ingredient/i,
+    });
+    userEvent.click(byIngredientButton);
+
+    const ingredientList = await screen.findAllByRole('img');
+    expect(ingredientList.length).toBe(LENGTH);
+
+    const rumBtn = await screen.findByRole('heading', {
+      name: /light rum/i,
+    });
+
+    userEvent.click(rumBtn);
+    expect(fetch).toBeCalledWith('https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=Light rum');
   });
 });
